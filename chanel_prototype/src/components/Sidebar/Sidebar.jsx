08068afe0ff30ccd89638b57447064a0e6fbb2fd
Aman <amanmalik3105@gmail.com>
2023-08-22 23:03:29 +0530

    Commencing Sidebar Component

---
+++ chanel_prototype/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Sidebar.css'; 
+
+const Sidebar = () => {
+  return (
+    <div className="container">
+      <button className='btn'>Home</button>
+      <button className='btn'>Guides</button>
+      <button className='btn'>Scent</button>
+      <button className='btn'>Contact</button>
+      <button className='btn'>Scent</button>
+      <button className='btn'>About Us</button>
+    </div>
+  );
+};
+
+export default Sidebar;
