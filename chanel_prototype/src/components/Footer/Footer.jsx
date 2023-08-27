316eb11c39bc749032d11cd3c242e2230b5cbfa9
Aman <amanmalik3105@gmail.com>
2023-08-27 17:07:46 +0530

    Footer Structure ready

--- chanel_prototype/src/components/Footer/Footer.jsx
+++ chanel_prototype/src/components/Footer/Footer.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import './Footer.css'
 const Footer = () => {
   return (
-    <div className='footer_container'>Footer</div>
+    <div className='footer_container'>
+        <p className='brand'>CHANEL</p>
+        <p className='tandc'>We've updated the dispute procedures in our Terms of Service (“Terms”).</p>
+    </div>
   )
 }
 
