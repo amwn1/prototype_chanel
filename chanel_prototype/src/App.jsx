91243d4cbfcaa4d2a0d919e0c131266bd657de74
Aman <amanmalik3105@gmail.com>
2023-08-27 16:57:22 +0530

    New component and resizing

--- chanel_prototype/src/App.jsx
+++ chanel_prototype/src/App.jsx
@@ -1,15 +1,19 @@
 
 import './App.css'
 import React from 'react'
-import SideBar from './components/sidebar/sidebar.jsx'
+import SideBar from './components/Sidebar/Sidebar.jsx'
 import Main from './components/MainPage/Main'
+import Footer from './components/Footer/Footer'
 function App() {
   
 
   return (
+    <div className="main_container">
     <div className='flex'>
       <SideBar />
       <Main/>
+    </div>
+      <Footer/>
     </div>
   )
 }
